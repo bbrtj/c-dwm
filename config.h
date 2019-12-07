501752c18175d87a6f8fecf746b51138f6347d06
brtastic <brtastic.dev@gmail.com>
2019-12-07 17:22:20 +0100

    Add uselessgap patch, improve configs

--- config.h
+++ config.h
@@ -2,11 +2,12 @@
 
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Droid Sans Mono:size=10" };
-static const char dmenufont[]       = "Droid Sans Mono:size=10";
+static const char *fonts[]          = { "Icons:size=13" };
+static const char dmenufont[]       = "Droid Sans Mono:size=11";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#cccccc";
@@ -15,11 +16,11 @@ static const char col_cyan[]        = "#4e82aa";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_gray2,  col_cyan  },
 };
 
 /* tagging */
-static const char *tags[] = { "&", "[", "{", "}", "(", "=" };
+static const char *tags[] = { "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -39,9 +40,9 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "til",      tile },    /* first entry is default */
-	{ "flo",      NULL },    /* no layout function means floating behavior */
-	{ "mon",      monocle },
+	{ "",      tile },    /* first entry is default */
+	{ "",      NULL },    /* no layout function means floating behavior */
+	{ "",      monocle },
 };
 
 /* key definitions */

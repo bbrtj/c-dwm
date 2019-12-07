de6f1b7fcc303dbf6272d0231a90483554bf9347
brtastic <brtastic.dev@gmail.com>
2019-12-07 17:23:46 +0100

    Remove monocle client count

--- dwm.c
+++ dwm.c
@@ -1103,16 +1103,16 @@ maprequest(XEvent *e)
 void
 monocle(Monitor *m)
 {
-	unsigned int n = 0;
-	Client *c;
+	// unsigned int n = 0;
+	// Client *c;
 
-	for (c = m->clients; c; c = c->next)
-		if (ISVISIBLE(c))
-			n++;
-	if (n > 0) /* override layout symbol */
-		snprintf(m->ltsymbol, sizeof m->ltsymbol, "[%d]", n);
-	for (c = nexttiled(m->clients); c; c = nexttiled(c->next))
-		resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
+	// for (c = m->clients; c; c = c->next)
+	// 	if (ISVISIBLE(c))
+	// 		n++;
+	// if (n > 0) /* override layout symbol */
+	// 	snprintf(m->ltsymbol, sizeof m->ltsymbol, "[%d]", n);
+	// for (c = nexttiled(m->clients); c; c = nexttiled(c->next))
+	// 	resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
 }
 
 void

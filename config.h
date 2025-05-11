8a8fe8f9ff71324196b41c1c3f3321b0124079ad
bbrtj <bbrtj.pro@gmail.com>
2025-05-11 07:50:35 +0200

    Fix whitespaces in config

--- config.h
+++ config.h
@@ -2,16 +2,16 @@
 
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 6;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int borderpx = 1;        /* border pixel of windows */
+static const unsigned int gappx = 6;        /* gaps between windows */
+static const unsigned int snap = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;        /* 0 means no systray */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "DejaVu Sans:size=10", "Icons:size=13" };
+static const int showsystray = 1;        /* 0 means no systray */
+static const int showbar = 1;        /* 0 means no bar */
+static const int topbar = 1;        /* 0 means bottom bar */
+static const char *fonts[] = { "DejaVu Sans:size=10", "Icons:size=13" };
 
 // Gruvbox
 static const char col_activebackground[] = "#282828";
@@ -21,39 +21,38 @@ static const char col_inactiveborder[] = "#98971a";
 static const char col_activetext[] = "#ebdbb2";
 static const char col_inactivetext[] = "#7c6f64";
 
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
+// fg, bg, border
+static const char *colors[][3] = {
 	[SchemeNorm] = { col_activetext, col_inactivebackground, col_inactiveborder }, // unselected window, title bar
 	[SchemeTitle] = { col_activetext, col_inactivebackground, col_inactiveborder }, // title bar
 	[SchemeDimmed] = { col_inactivetext, col_inactivebackground, col_inactiveborder }, // non-active tags on the tagbar
-	[SchemeSel]  = { col_activetext, col_activebackground, col_activeborder }, // currently selected window
+	[SchemeSel] = { col_activetext, col_activebackground, col_activeborder }, // currently selected window
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", ""};
+static const char *tags[] = { "", "", "", "", "", "", "", "" };
 static const char tagclients[] = "";
 static const char tagempty[] = "";
 
+// xprop(1):
+// WM_CLASS(STRING) = instance, class
+// WM_NAME(STRING) = title
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "OvoPlayer", NULL,       "MiniPlayer",       0,       1,           -1 },
 	{ "feh",       NULL,       NULL,               0,       1,           -1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
+static const float mfact = 0.50; /* factor of master area size [0.05..0.95] */
+static const int nmaster = 1; /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
-	/* symbol     arrange function */
+	/* symbol   fn       hides topbar */
 	{ "",      deck,    0 },
 	{ "",      tile,    0 },
-	{ "",      NULL,    0 },    /* no layout function means floating behavior */
+	{ "",      NULL,    0 }, /* no layout function means floating behavior */
 	{ "",      monocle, 0 },
 	{ "",      picture, 1 },
 };
@@ -69,8 +68,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
-static const char *termcmd[]  = { "st", NULL };
-static const char *pstatustimecmd[]  = { "pstatus", "--type", "time", "--update", NULL };
+static const char *termcmd[] = { "st", NULL };
+static const char *pstatustimecmd[] = { "pstatus", "--type", "time", "--update", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -138,9 +137,9 @@ static Button buttons[] = {
  * r means right
  * ud means up and down
  */
-static const char *ovoplaypause[]  = { "ovoplayerctrl", "--playpause", NULL };
-static const char *ovonext[]  = { "ovoplayerctrl", "--next", NULL };
-static const char *ovoprev[]  = { "ovoplayerctrl", "--previous", NULL };
+static const char *ovoplaypause[] = { "ovoplayerctrl", "--playpause", NULL };
+static const char *ovonext[] = { "ovoplayerctrl", "--next", NULL };
+static const char *ovoprev[] = { "ovoplayerctrl", "--previous", NULL };
 static Gesture gestures[] = {
 	{ "u",  spawn, {.v = ovoplaypause} },
 	{ "dl",  spawn, {.v = ovoprev} },
